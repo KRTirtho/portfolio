5bca0debf7ca59a8153ff11509c3eca7e8084270
Kingkor Roy Tirtho <krtirtho@gmail.com>
2023-11-03 09:12:55 +0600

    feat: initialized nextjs

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Portfolio | Kingkor Roy Tirtho',
+  description: 'Hi, I\'m a full-stack and Flutter developer from Bangladesh with knowledge in multiple languages and frameworks.',
+  creator: 'Kingkor Roy Tirtho',
+  applicationName: 'Portfolio',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
